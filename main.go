3094b365929a7ee2326211efb65d09c49ff8a1a4
Hady Willi <hady.willi@outlook.com>
2023-06-27 22:52:06 +0800

    feat: config read vendoring

--- main.go
+++ main.go
@@ -3,11 +3,20 @@ package main
 import (
 	"github.com/hjwalt/flows"
 	"github.com/hjwalt/flows-deployment/function"
+	"github.com/hjwalt/flows/configuration"
+	"github.com/hjwalt/flows/format"
 	"github.com/hjwalt/runway/environment"
 )
 
+type Config struct {
+	Name   string `yaml:"name"`
+	Number int64  `yaml:"number"`
+}
+
 func main() {
 	m := flows.Main()
+
+	configuration.Read("config.yaml", format.Yaml[Config]())
 
 	m.Register("word-count", function.WordCountRun)
 	m.Register("word-remap", function.WordRemapRun)
